5cc02e7c34c1066534b32f0f58082ba526d319c6
Stspring <St.spring@tutamail.com>
2024-10-12 21:10:18 +0300

    Z14

--- Z10.py
+++ Z10.py
@@ -12,7 +12,7 @@ b = int(data[1])
 c = int(data[2])
 d = int(data[3])
 x3 = " "
-x1 =  " "
+x1 = " "
 x2 = " "
 for i in range(-100,100+1):
     if (a*i**3+b*i**2+c*i+d)==0:
@@ -27,11 +27,11 @@ for i in range(-100,100+1):
                     break
         break
 data_output = open('output.txt','w')
-if x3 != ' ':
+if x3 != ' ': # если х не равен 0, выводим 3 хса
     data_output.write(str(x1)+' '+str(x2)+' '+str(x3))
 elif x2 !=' ':
      data_output.write(str(x1)+' '+str(x2))
-else:
+else: 
      data_output.write(str(x1))
 data_input.close()
 data_output.close()
